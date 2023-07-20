0d87d9ddcba97014eebc564e69a3554de0ab0204
sneyarose <84334991+sneyarose@users.noreply.github.com>
2023-07-20 10:33:54 +0530

    Auth update

--- auth.php
+++ auth.php
@@ -11,9 +11,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Check if the username and password match the desired values
     if (array_key_exists($username, $users) && $users[$username] === $password) {
-        // Successful login, redirect to the desired page
-        header("Location: success.html");
-        exit();
+        // Successful login, redirect to the appropriate page
+        if ($username === "Yadu") {
+            header("Location: admin.html");
+            exit();
+        } elseif ($username === "Sneya") {
+            header("Location: login.html");
+            exit();
+        }
     } else {
         // Failed login, redirect back to the login page
         header("Location: login.html");
@@ -21,3 +26,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
